f1aca0effca486e04211edc128c85a82aea1eee5
Mahfuzz-z <98677539+Mahfuzz-z@users.noreply.github.com>
2022-06-13 19:13:10 +0600

    Update login.php

--- login.php
+++ login.php
@@ -28,23 +28,22 @@
                 
 
             </form>
-            
-
-
-
+            <?php
+            if(isset($_POST['stlogin_button'])){
+                Header("Location:student_login.php");
+            }
+            if(isset($_POST['faculty_login_button'])){
+                Header("Location:faculty_login.php");
+            }
+            if(isset($_POST['admin_login_button'])){
+                Header("Location:admin_login.php");
+            }
+            ?>
+     
             </div>
-
-            
-      
       
    </div>
 
-
-
-
-
-
-  
 
 
     <!-- Optional JavaScript -->
